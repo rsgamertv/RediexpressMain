e19ae5a19cf8a7be08b4933d237f5cc6b34d274a
rsgamertv <Wersuser2@gmail.com>
2024-03-09 12:10:20 +0300

    аввыава

---
+++ lib/Authorization/Authorization.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Authorization extends StatefulWidget{
+  @override
+  State<Authorization> createState() => _AuthorizationState();
+}
+
+class _AuthorizationState extends State<Authorization> {
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
