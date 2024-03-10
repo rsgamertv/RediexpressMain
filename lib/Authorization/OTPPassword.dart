eb73de98bca08cf2166aec329262d2a77529093d
rsgamertv <Wersuser2@gmail.com>
2024-03-10 19:51:24 +0300

    forgot password fixed(added route to OTPPassword

---
+++ lib/Authorization/OTPPassword.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class OTPPassword extends StatefulWidget{
+  @override
+  State<OTPPassword> createState() => _OTPPasswordState();
+}
+
+class _OTPPasswordState extends State<OTPPassword> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+          child: Column(
+            children: [
+              
+            ],
+          ),
+        ),
+    );
+  }
+}
